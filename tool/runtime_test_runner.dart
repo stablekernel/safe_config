da05896f315eb0ee0e4d39bcf2bc98f2452bf2c7
Joe Conway <joeconway3@gmail.com>
2020-01-15 16:40:22 -0500

    fix test runner

--- tool/runtime_test_runner.dart
+++ tool/runtime_test_runner.dart
@@ -4,8 +4,6 @@ import 'dart:io';
 import 'package:runtime/runtime.dart';
 
 Future main(List<String> args) async {
-  print(Directory("/home/travis/.pub-cache/").listSync(recursive: true).map((f) => f.path).join("\n"));
-
   final blacklist = ["no_default_constructor_test.dart"];
   final testFiles = Directory.fromUri(Directory.current.uri.resolve("test/"))
       .listSync(recursive: true)
@@ -33,17 +31,19 @@ Future main(List<String> args) async {
     await bm.build();
 
     print("${makePrompt()} Running tests derived from ${f.path}...");
-    final result = await Process.run("./run", [],
-        workingDirectory:
-            Directory.current.uri.toFilePath(windows: Platform.isWindows),
-        environment: {
-          "TEST_BOOL": "true",
-          "TEST_DB_ENV_VAR": "postgres://user:password@host:5432/dbname",
-          "TEST_VALUE": "1"
-        });
-    print(result.stdout);
-    print(result.stderr);
-    if (result.exitCode != 0) {
+    print("${makePrompt()} Running tests derived from ${f.path}...");
+    final result = await Process.start("dart", ["test/main_test.dart"],
+      workingDirectory:
+      ctx.buildDirectoryUri.toFilePath(windows: Platform.isWindows),
+    environment: {
+      "TEST_BOOL": "true",
+      "TEST_DB_ENV_VAR": "postgres://user:password@host:5432/dbname",
+      "TEST_VALUE": "1"
+    });
+    stdout.addStream(result.stdout);
+    stderr.addStream(result.stderr);
+
+    if (await result.exitCode != 0) {
       exitCode = -1;
       failCounter++;
       print("Tests FAILED in ${f.path}.");
@@ -51,8 +51,7 @@ Future main(List<String> args) async {
       passCounter++;
     }
     print("${makePrompt()} Completed tests derived from ${f.path}.");
-    await bm.clean();
-    File.fromUri(Directory.current.uri.resolve("run")).deleteSync();
+//    await bm.clean();
     remainingCounter--;
   }
 }
